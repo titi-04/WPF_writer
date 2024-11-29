3ebef31533978b35e922fa10a42a6e27e2622744
4b2g0039@office.stust.edu.tw <4b2g0039@office.stust.edu.tw>
2024-11-29 21:36:27 +0800

    新增命令綁定及按鈕屬性，調整工具列寬度
    在 MyDocumentViewer.xaml 中新增 Window.CommandBindings 元素，綁定 ApplicationCommands.New、ApplicationCommands.Open 和 ApplicationCommands.Save 命令，分別對應 NewCommand_Executed、OpenCommand_Executed 和 SaveCommand_Executed 方法。修改 applicationToolBar 的寬度，並新增 Margin 屬性。將 newButton、openButton 和 saveButton 按鈕分別綁定到 ApplicationCommands.New、ApplicationCommands.Open 和 ApplicationCommands.Save 命令。新增 trashButton 按鈕的 x:Name 屬性。在 MyDocumentViewer.xaml.cs 中，新增 System.Collections.Generic 和 System.Windows.Documents 的 using 指令。為 fontSizeComboBox 新增字體大小選項，預設值為索引 3。新增 NewCommand_Executed、OpenCommand_Executed 和 SaveCommand_Executed 方法，其中 NewCommand_Executed 方法會創建並顯示一個新的 MyDocumentViewer 視窗。

--- WPF_4/MyDocumentViewer.xaml.cs
+++ WPF_4/MyDocumentViewer.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Documents;
 using System.Windows.Media;
 
 namespace WPF_4
@@ -19,7 +21,25 @@ namespace WPF_4
                 fontFamilyComboBox.Items.Add(fontFamily.Source);
             }
             fontFamilyComboBox.SelectedItem = "DFPBiaoKaiShu-B5";
-            //或者fontFamilyComboBox.
+            //或者fontFamilyComboBox.SelectedIndex = 10;
+            fontSizeComboBox.ItemsSource = new List<double>() { 8, 9, 10, 12, 14, 16, 18, 20, 22, 24, 32, 40, 50, 60, 80, 100 };
+            fontSizeComboBox.SelectedIndex = 3;//預設值
+        }
+
+        private void NewCommand_Executed(object sender, System.Windows.Input.ExecutedRoutedEventArgs e)
+        {
+            MyDocumentViewer myDocumentViewer = new MyDocumentViewer();
+            myDocumentViewer.Show();
+        }
+
+        private void OpenCommand_Executed(object sender, System.Windows.Input.ExecutedRoutedEventArgs e)
+        {
+
+        }
+
+        private void SaveCommand_Executed(object sender, System.Windows.Input.ExecutedRoutedEventArgs e)
+        {
+
         }
     }
 }
