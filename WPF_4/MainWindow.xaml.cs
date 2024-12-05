54e63f4b487a764edca898aef1311b844a596a1a
4b2g0039@office.stust.edu.tw <4b2g0039@office.stust.edu.tw>
2024-12-06 02:29:22 +0800

    新增按鈕點擊事件與文字變更事件處理程序
    在 `MainWindow.xaml.cs` 中，新增了一個 `Button_Click` 事件處理程序。當按鈕被點擊時，會創建並顯示一個 `MyDocumentViewer` 視窗。
    
    在 `MyDocumentViewer.xaml` 中，為 `RichTextBox` 控制項新增了一個 `TextChanged` 事件處理程序 `rtbEditor_TextChanged`。

--- WPF_4/MainWindow.xaml.cs
+++ WPF_4/MainWindow.xaml.cs
@@ -25,8 +25,10 @@ namespace WPF_4
             InitializeComponent();
         }
 
+        //Click觸發一個事件
         private void Button_Click(object sender, RoutedEventArgs e)
         {
+            //呼叫主視窗
             MyDocumentViewer myDocumentViewer = new MyDocumentViewer();
             myDocumentViewer.Show();//Show有從屬關係(可開多個檔案) ShowDialog無從屬關係(關閉檔案才能再開新檔案)
         }
